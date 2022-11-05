b27a206c6970ee700aa2d79bc06e0f3daa392add
Fuzail22 <102959853+Fuzail22@users.noreply.github.com>
2022-11-05 11:30:14 +0530

    changed css for overflow text in the buttons

---
+++ timer.js
@@ -0,0 +1,31 @@
+var x = new Audio("time_up_s7.mp3");
+function playAudio() {
+  x.play();
+}
+function pauseAudio() {
+  x.pause();
+}
+function dateDiff() {
+  let SelectedDateTime = document.getElementById("dateTime").value;
+
+  const inter = setInterval(() => {
+    let currentDate = new Date();
+    let timerDate = new Date(SelectedDateTime); //converts string to date type
+    let diff = timerDate.getTime() - currentDate.getTime(); //returns diff in milliseconds
+    
+    if (diff < 1000) {
+      playAudio();
+      clearInterval(inter);
+    }
+    
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / 1000 / (60 * 60));
+    let mins = Math.floor((diff % (1000 * 60 * 60)) / 1000 / 60);
+    let secs = Math.floor((diff % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("mins").innerHTML = mins;
+    document.getElementById("secs").innerHTML = secs;
+  });
+}
