eebdba7b507e77a5991d6038b5d8f53786706239
Fuzail22 <102959853+Fuzail22@users.noreply.github.com>
2023-11-16 20:53:18 +0530

    path changed

--- timer.js
+++ timer.js
@@ -7,17 +7,21 @@ function pauseAudio() {
 }
 function dateDiff() {
   let SelectedDateTime = document.getElementById("dateTime").value;
-
+  if (!SelectedDateTime) {
+    console.log("Time not selected");
+    return;
+  }
+  console.log("selection is " + SelectedDateTime);
   const inter = setInterval(() => {
     let currentDate = new Date();
     let timerDate = new Date(SelectedDateTime); //converts string to date type
     let diff = timerDate.getTime() - currentDate.getTime(); //returns diff in milliseconds
-    
+
     if (diff < 1000) {
       playAudio();
       clearInterval(inter);
     }
-    
+
     let days = Math.floor(diff / (1000 * 60 * 60 * 24));
     let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / 1000 / (60 * 60));
     let mins = Math.floor((diff % (1000 * 60 * 60)) / 1000 / 60);
